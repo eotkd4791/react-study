6d61977a17b1b4a35ed24f6d267d9ff941137e74
eotkd4791 <eotkd4791@gmail.com>
2021-01-12 00:06:44 +0900

    react

---
+++ 컴포넌트/react-component/src/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import Counter from './Counter';
+import EventPractice from './EventPractice.js';
+
+const App = () => {
+  return <EventPractice />;
+};
+
+export default App;
