9f5ec6689f2089aaf6119ae4f1e267da805a4305
eotkd4791 <eotkd4791@gmail.com>
2021-03-28 22:31:18 +0900

    불필요한 파일 제거

--- react-component/src/App.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import Counter from './Counter';
-import EventPractice from './EventPractice.js';
-
-const App = () => {
-  return <EventPractice />;
-};
-
-export default App;
