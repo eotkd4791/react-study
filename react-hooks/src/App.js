3a6660bbaa4984bc02a417ff69e05433a4533cef
eotkd4791 <eotkd4791@gmail.com>
2021-03-31 07:24:02 +0900

    React Hooks

---
+++ react-hooks/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+// import EffectInfo from './EffectInfo.jsx';
+// import ReducerCounter from './ReducerCounter.jsx';
+// import StateCounter from './StateCounter.jsx';
+// import ReducerInfo from './ReducerInfo';
+// import StateAverage from './StateAverage';
+// import MemoAverage from './MemoAverage';
+// import CallbackAverage from './CallbackAverage';
+import RefAverage from './RefAverage';
+
+const App = () => {
+  return <RefAverage/>
+};
+
+export default App;
