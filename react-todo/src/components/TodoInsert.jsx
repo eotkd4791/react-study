fb401537a8d888629c1de7fe5a37ef6223f3db9e
eotkd4791 <eotkd4791@gmail.com>
2021-04-01 23:04:47 +0900

    TodoList App에 styled-components 적용해보기

--- react-todo/src/components/TodoInsert.jsx
+++ react-todo/src/components/TodoInsert.jsx
@@ -1,6 +1,43 @@
 import React, {useState, useCallback} from 'react';
+import styled from 'styled-components';
 import { MdAdd } from 'react-icons/md';
-import './TodoInsert.scss';
+// import 'TodoInsert.scss';
+
+const Input = styled.input`
+  background: none;
+  outline: none;
+  border: none;
+  padding: 0.5rem;
+  font-size: 1.125rem;
+  line-height: 1.5;
+  color: #fff;
+  flex: 1;
+  &:placeholder {
+    color: #dee2e6;
+  }
+`;
+
+const Button = styled.button`
+  background: none;
+  outline: none;
+  border: none;
+  background: #868e96;
+  color: #fff;
+  padding: 0.7rem 1rem;
+  font-size: 1.5rem;
+  display: flex;
+  align-items: center;
+  cursor: pointer;
+  transition: 1ms background ease-in;
+  &:hover {
+    background: #adb5bd;
+  }
+`;
+
+const Form = styled.form`
+  display: flex;
+  background-color: #495057;
+`;
 
 const TodoInsert = ({ onInsert }) => {
   const [value, setValue] = useState('');
@@ -19,16 +56,16 @@ const TodoInsert = ({ onInsert }) => {
   );
 
   return (
-    <form className="TodoInsert" onSubmit={onSubmit}>
-      <input 
+    <Form>
+      <Input 
         placeholder="할 일을 입력하세요."
         value={value}
         onChange={onChange}
       />
-      <button type="submit">
+      <Button type="submit">
         <MdAdd/>
-      </button>
-    </form>
+      </Button>
+    </Form>
   )
 };
 
--- react-todo/src/components/TodoTemplate.jsx
+++ react-todo/src/components/TodoTemplate.jsx
@@ -1,12 +1,34 @@
 import React from 'react';
-import './TodoTemplate.scss';
+import styled from 'styled-components';
+// import './TodoTemplate.scss';
+
+const TodoTemplateContainer = styled.div`
+  width: 512px;
+  margin: 6rem auto 0;
+  border-radius: 4px;
+  overflow: hidden;
+`;
+
+const AppTitle = styled.div`
+  background-color: #22b8cf;
+  color: #fff;
+  height: 4rem;
+  font-size: 1.5rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Content = styled.div`
+  background-color: #fff;
+`;
 
 const TodoTemplate = ({ children }) => {
   return (
-    <div className="TodoTemplate">
-      <div className="app-title">일정 관리</div>
-      <div className="content">{children}</div>
-    </div>
+    <TodoTemplateContainer>
+      <AppTitle>일정 관리</AppTitle>
+      <Content>{children}</Content>
+    </TodoTemplateContainer>
   );
 };
 
