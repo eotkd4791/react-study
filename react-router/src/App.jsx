6bfea81a977a3e0f1ec2e94d7d48976c5a150a99
eotkd4791 <eotkd4791@gmail.com>
2021-04-20 01:04:18 +0900

    라우터 부가 기능 history

--- react-router/src/App.jsx
+++ react-router/src/App.jsx
@@ -3,6 +3,7 @@ import { Route, Link } from 'react-router-dom';
 import Home from './Home';
 import About from './About';
 import Profile from './Profile';
+import HistorySample from './HistorySample';
 
 const App = () => {
   return (
@@ -15,16 +16,20 @@ const App = () => {
           <Link to="/about">소개</Link>
         </li>
         <li>
-          <Link to="profile/stef">stef 프로필</Link>
+          <Link to="/profile/stef">stef 프로필</Link>
         </li>
         <li>
-          <Link to="profile/eden">eden 프로필</Link>
+          <Link to="/profile/eden">eden 프로필</Link>
+        </li>
+        <li>
+          <Link to="/history">History 예제</Link>
         </li>
       </ul>
       < hr />
       <Route path="/" component={Home} exact={true}/>
       <Route path={['/about','/info']} component={About} />
       <Route path="/profile/:username" component={Profile} />
+      <Route path="/history" component={HistorySample} />
     </div>
   );
 };
