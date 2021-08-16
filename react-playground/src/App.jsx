cb6c0068703e22df3b1b572e5a1ebf1d2d0f2406
eotkd4791 <eotkd4791@gmail.com>
2021-08-16 23:28:32 +0900

    공식문서 example

---
+++ react-playground/src/App.jsx
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import Clock from './Clock';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+      </header>
+      <main>
+      <Clock />
+      </main>
+    </div>
+  );
+}
+
+export default App;
---
+++ react-playground/src/Clock.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      date: new Date()
+    };
+  }
+
+  tick() {
+      this.setState({
+        date: new Date()
+      });
+  }
+
+  componentDidMount() {
+    this.timeID = setInterval(() => this.tick(), 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timeID);
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Hello, world!</h1>
+        <h2>It is {this.state.date.toLocaleString()}</h2>
+      </div>
+    );
+  }
+}
+
+export default Clock;
